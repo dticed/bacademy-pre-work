789475f7b9e7605bb0c405861e0db22b3c731b94
dticed <lucaspierrealencar@gmail.com>
2021-08-18 08:59:33 -0300

    resolucao dia1 do fdaciuk

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -6,26 +6,8 @@ app.innerHTML = `<h1>B. Academy</h1>
 `
 
 const link = document.querySelector('[data-js="link"]');
-link.innerHTML = "Click aqui para remover e adicionar o app"
 link.addEventListener('click', (e) => {
   e.preventDefault();
-  if (app.innerHTML.length === 0) {
-    addApp(app);
-    link.innerHTML = "Click para remover o app"
-  } else {
-    app.innerHTML = "";
-    link.innerHTML = "Click para adicionar o app"
-  }
-}, false);
+  app.classList.toggle('hide');
+})
 
-function addApp(element) {
-  element.innerHTML = `<h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-  `
-}
-
-//Desafio 4
-// Crie um link no HTML (fora da div .app), e adicione à ele um
-// evento de clique. O clique nesse botão deverá alternar a
-// visibilidade do .app: se o .app estiver visível, ele deverá
-// ser escondido. Se estiver escondido, o clique deve exibí-lo.
