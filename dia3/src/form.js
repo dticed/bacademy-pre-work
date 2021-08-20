ca4fcc499fa80977864f9518ff1396948ed98277
dticed <lucaspierrealencar@gmail.com>
2021-08-20 12:12:42 -0300

    resolucao exercicio 2 - dia 3

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,5 +1,8 @@
-const body = document.querySelector('body');
+const app = document.querySelector('.app');
+const form = document.querySelector('[data-js="form"]');
 const nome = document.querySelector('[data-js="nome"]')
+const select = document.createElement('select');
+const boxColors = document.createElement('div');
 
 const p = document.createElement('p');
 
@@ -10,22 +13,13 @@ nome.addEventListener('input', (e) => {
   p.textContent = "";
 
   if (e.target.value !== " " || e.target.value !== "") {
-    //p.textContent = e.target.value;
     string += e.target.value;
   }
 
   const arr = string.split(" ");
 
-  // arr.map(element => {
-  //   if(wordToCheck.includes(element)) {
-  //     element.toLowerCase();
-  //   } else {
-  //     element.charAt(0).toUpperCase();
-  //   }
-  // })
-
-  for(let i = 0; i < arr.length; i++) {
-    if(wordToCheck.includes(arr[i])) {
+  for (let i = 0; i < arr.length; i++) {
+    if (wordToCheck.includes(arr[i])) {
       arr[i] = arr[i].toLowerCase();
     } else {
       arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
@@ -36,5 +30,33 @@ nome.addEventListener('input', (e) => {
     p.textContent += e + " ";
   })
 
-  body.appendChild(p)
+  app.appendChild(p)
 })
+
+const colors = ['red', 'blue', 'green', 'black', 'purple']
+
+select.setAttribute('name', 'color-select')
+select.setAttribute('multiple', '')
+select.setAttribute('size', 5)
+
+colors.forEach((e) => {
+  const option = document.createElement('option');
+  option.setAttribute('style', `color:${e}`)
+  option.textContent = e;
+  select.appendChild(option)
+})
+
+select.addEventListener('change', (e) => {
+  boxColors.innerHTML = "";
+  [...e.target.selectedOptions].forEach(el => {
+    const div = document.createElement('div');
+    if (el.selected) {
+      div.setAttribute('style', `background-color:${el.value}; height:100px; width:100px;`)
+      boxColors.appendChild(div);
+    }
+  })
+})
+
+
+form.appendChild(select)
+app.appendChild(boxColors);
