f1335a7e8515b42b927760fc5a1cdc2285e57290
dticed <lucaspierrealencar@gmail.com>
2021-08-22 12:58:03 -0300

    add delete request

--- dia4/app/cars.js
+++ dia4/app/cars.js
@@ -50,7 +50,6 @@ async function cadastrarCarro(imagem, brandModel, year, plate, color) {
     console.log("Não foi possível cadastrar o carro.")
   }
   getCars()
-
 }
 
 async function getCars() {
@@ -71,15 +70,17 @@ function renderCars(cars) {
     if (span2) {
       span2.innerHTML = ""
     }
-    console.log(cars)
     cars.forEach(car => {
       const deleteButton = document.createElement('button');
       deleteButton.classList.add('delete-button')
       deleteButton.textContent = "Delete"
-      //deleteButton.addEventListener('click', deleteCar());
+      deleteButton.dataset.plate = car.plate;
       const tr = document.createElement('tr')
       for (let key in car) {
         const th = document.createElement('th')
+        if (key === "plate") {
+          tr.dataset.plate = car[key];
+        }
         if (key === "image") {
           const img = document.createElement('img')
           img.src = car[key]
@@ -90,14 +91,31 @@ function renderCars(cars) {
           tr.appendChild(th)
         }
       }
+      deleteButton.addEventListener('click', deleteCar);
       tr.appendChild(deleteButton)
       tbody.appendChild(tr)
     })
   }
 }
 
-function deleteCar() {
-  console.log('teste')
+function deleteCar(e) {
+  console.log(e.target.dataset.plate)
+  const plate = e.target.dataset.plate
+  const tr = document.querySelector(`tr[data-plate="${plate}"]`)
+  tbody.removeChild(tr)
+  const result = deleteCarDB({ plate })
+
+}
+
+async function deleteCarDB(data) {
+  await fetch('http://localhost:3333/cars', {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+  getCars()
 }
 
 function loadApp() {
